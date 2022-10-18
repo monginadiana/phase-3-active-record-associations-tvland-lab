63982ee563104457022c6c0a3f0615143b461a18
monginadiana <dianahmongina2@gmail.com>
2022-10-18 11:54:15 +0300

    commit tree c3e39a6bafd466b78b2a15ac77e12b9e7eed56db

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -1,5 +1,11 @@
 class CreateCharacters < ActiveRecord::Migration[6.1]
   def change
-    # Your code here
+    create_table :shows do |t|
+      # primary key of :id is created for us!
+      t.string :name
+      t.integer :show_id
+      t.integer :actor_id
+      t.string :genre
+    end
   end
 end
